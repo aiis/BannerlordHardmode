792e2953d5f98a643c2cfa498721ef27542c20dd
aiis <aiis@users.noreply.github.com>
2020-04-10 23:31:33 -0500

    Fixed bugs - wasn't running on subsequent captures; and couldn't pass below zero when taking a percentage of influence

--- BannerlordHardmode/HardmodePlayerCaptivityModel.cs
+++ BannerlordHardmode/HardmodePlayerCaptivityModel.cs
@@ -9,9 +9,11 @@ namespace BannerlordHardmode
         private float lastApplied = 0;
         public override string CheckCaptivityChange(float dt)
         {
-            if (Campaign.CurrentTime - lastApplied == 24f | lastApplied == 0)
+            if (Campaign.CurrentTime - lastApplied >= 24f | lastApplied == 0)
             {
-                Actions.ChangeInfluence.Apply(Hero.MainHero, -Math.Abs(Hero.MainHero.Clan.Influence * 0.25f));
+                float influenceChange = Math.Abs(Hero.MainHero.Clan.Influence * 0.25f);
+                float actualInfluenceChange = influenceChange >= 5 ? influenceChange : 5;
+                Actions.ChangeInfluence.Apply(Hero.MainHero, -actualInfluenceChange);
                 Actions.ChangeRenown.Apply(Hero.MainHero, -(Hero.MainHero.Clan.Renown * 0.15f), Convert.ToSingle(Campaign.Current.Models.ClanTierModel.GetRequiredRenownForTier(Hero.MainHero.Clan.Tier)));
                 lastApplied = Campaign.CurrentTime;
             }
