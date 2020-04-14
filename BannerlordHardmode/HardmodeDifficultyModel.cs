ac06a00850efca014169eec023a4c5c0a25ce3f5
aiis <aiis@users.noreply.github.com>
2020-04-13 23:12:14 -0500

    Removed modded party speed leftover from testing

--- BannerlordHardmode/HardmodeDifficultyModel.cs
+++ BannerlordHardmode/HardmodeDifficultyModel.cs
@@ -1,5 +1,4 @@
 ﻿using TaleWorlds.CampaignSystem.SandBox.GameComponents;
-using TaleWorlds.CampaignSystem;
 
 namespace BannerlordHardmode
 {
@@ -19,28 +18,5 @@ namespace BannerlordHardmode
         {
             return 1f;
         }
-
-        public override float GetPlayerMapMovementSpeedBonusMultiplier()
-        {
-            switch (CampaignOptions.PlayerMapMovementSpeed)
-            {
-                case CampaignOptions.Difficulty.VeryEasy:
-                    return 1f;
-                case CampaignOptions.Difficulty.Easy:
-                    return .5f;
-                case CampaignOptions.Difficulty.Realistic:
-                    return 0.0f;
-                default:
-                    return 0.0f;
-            }
-        }
-    }
-
-    public enum HardmodeDifficulty
-    {
-        VeryEasy,
-        Easy,
-        Realistic,
-        Hardmode,
     }
 }
